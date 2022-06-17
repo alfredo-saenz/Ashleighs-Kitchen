5bd99fa5e55763921c87003d650368cd9c70f2ae
alfredo-saenz <alfredosaenz104@gmail.com>
2022-06-17 18:21:39 -0500

    Made components

---
+++ src/components/SearchForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SearchForm(props) {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+            </form>
+
+        </div>
+    );
+}
+
+export default SearchForm;
